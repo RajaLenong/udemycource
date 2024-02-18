f36388b13acc13f8aee7aa3b7f6afcd8804d541e
Raja Lenong <learning@rajalenong.com>
2024-02-19 06:03:20 +0700

    ConsoleTest

---
+++ ConsoleTest1/ConsoleTest1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka1;
+            int angka2;
+            int hasil;
+
+            angka1 = 10;
+            angka2 = 5;
+            hasil = (angka1 + angka2);
+
+            Console.WriteLine("Hasil dari penjumlahan " + angka1.ToString() + " ditambah " + angka2.ToString() + " adalah " + hasil.ToString());
+            Console.ReadKey();
+        }
+    }
+}

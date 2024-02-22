3170246c2beaec7379e91588edb36fddfc8db8ff
Raja Lenong <learning@rajalenong.com>
2024-02-22 22:56:31 +0700

    konversi CTOF

---
+++ KonversiCtoF/KonversiCtoF/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonversiCtoF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string temperatur = textBox1.Text;
+            double konversi = Convert.ToDouble(temperatur);
+            double rumus_c_to_f = (konversi * 1.8) + 32;
+            label3.Text = rumus_c_to_f.ToString() + " F";
+        }
+    }
+}
